3457cebac9b4a3717b273dd8f3f030aef6861c4d
sandeep kumar sahoo <chandansandeep2000@gmail.com>
2023-12-24 11:49:59 +0530

    Routes Setup Complete

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,47 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+/** Import All Components */
+import Username from './components/Username'
+import Reset from './components/Reset';
+import Register from './components/Register';
+import Recovery from './components/Recovery';
+import Profile from './components/Profile'
+import Password from './components/Password'
+import PageNotFound from './components/PageNotFound'
+
 
 /** root Routes*/
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Root Route</div>
+    element: <Username/>
   },
   {
     path: '/register',
-    element: <div>Register Route</div>
+    element: <Register></Register>
   },
   {
-    path: '/login',
-    element: <div>Login Route</div>
-  }
+    path: '/password',
+    element: <Password></Password>
+  },
+  {
+    path: '/profile',
+    element: <Profile></Profile>
+  },
+  {
+    path: '/recovery',
+    element: <Recovery></Recovery>
+  },
+  {
+    path: '/reset',
+    element: <Reset></Reset>
+  },
+  {
+    path: '/*',
+    element: <PageNotFound></PageNotFound>
+  },
+  
 ])
 function App() {
   return (
